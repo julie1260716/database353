3a3b0992507cc67a03f23c3a012d266c97430a12
julie1260716 <39203855+julie1260716@users.noreply.github.com>
2018-11-26 10:47:55 -0800

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,5 @@
+<?php
+//Step1: Open Connection 
+ $db = mysqli_connect('127.0.0.1:3306','root','lulu40007795', 'wdc353_2')
+ or die('Error connecting to MySQL server.');
+?>
--- home_page/home_page.php
+++ home_page/home_page.php
@@ -1,52 +1,4 @@
-<?php
-	if(isset($_COOKIE["cli_num"]))
-    	$id = $_COOKIE["cli_num"];
-    if(isset($_COOKIE["cli_fname"]))
-    	$fname = $_COOKIE["cli_fname"];
-    if(isset($_COOKIE["cli_lname"]))
-        $lname = $_COOKIE["cli_lname"];
-    if(isset($_COOKIE["cli_dob"]))
-        $dob = $_COOKIE["cli_dob"];
-    if(isset($_COOKIE["cli_email"]))
-            $phone = $_COOKIE["cli_email"];
-    if(isset($_COOKIE["cli_phone"]))
-        $email = $_COOKIE["cli_phone"];
-	if(isset($_COOKIE["cli_add"]))
-	    $add = $_COOKIE["cli_add"];  
 
-	if(isset($_COOKIE["accounts"])) {
-	    $cookie_account = unserialize($_COOKIE['accounts']);
-	}
-	else {
-		$cookie_account = array(
-			40023289 => 5000,
-			50023289 => 500,
-			997789 => 1000);
-
-		$id = 123456789;
-		$fname = "John";
-
-		$acc_list = "accounts";
-		$cookie_acc = serialize($cookie_account);
-
-		$cookie_receive = array(
-			12345 => 500,
-			778899 => 10000);
-		$acc_rec_list = "acc_rec_list";
-		$rec_list = serialize($cookie_receive);
-
-		$bills = array(
-			"Electricity" => 300, 
-			"Rent" => 700);
-		$bill_list = "bills";
-		$cookie_bills = serialize($bills);
-		
-		setcookie($acc_list, $cookie_acc, time() + (86400 * 30), "/"); // 86400 = 1 day
-		setcookie($bill_list, $cookie_bills, time() + (86400 *30), "/");
-		setcookie($acc_rec_list, $rec_list, time() + (86400 * 30), "/"); // 86400 = 1 day
-	}
-
-?>
 <!DOCTYPE html>
 <html>
     
@@ -62,11 +14,11 @@
         <h1>BANK.</h1>
         <h2>Welcome to Online Banking</h2>
 
-        <form>
+        <form action="../signup/signup.php">
 
 			<div class="flex-container">   
 				<div class = "account">   
-					<p class="subtitle">Hello <?php echo $fname ?></p>
+					<p class="subtitle">Hello </p>
 					    <ul> 
 						   <li><a href="../send_money/send_money.php">Send money</a></li>
 						   <li><a href="../transfer_money/transfer_money.php">Transfer money</a></li>
@@ -78,23 +30,20 @@
 		            <br>
 					<br>
 
-					<label>Account</label>
-					<table class="display_account"> 
-						<?php foreach ($cookie_account as $key => $value) { ?>
+					<table class="display_account"> Account
+						<tr class="account_info">
+							<td id="account_number">40023289</td>
+							<td id="account_balance">CDN 9999</td>
+						</tr>
 
-					        <tr>
-					           <td><?php echo $key ?></td>
-					           <td><?php echo $value ?></td>
-					        </tr>
-
-					       	<?php } ?>
+						<tr>
+						</tr>
 					</table>
 
-					<label>Credit card</label>
-					<table class="credit_card_line">
-						<tr>
-							<td>5004789</td>
-							<td>-150</td>
+					<table class="credit_card_line">Credit Card
+						<tr class="credit_info">
+							<td id="credit_number">5004789</td>
+							<td id="credit_balance">-150</td>
 						</tr>
 					</table>
 
